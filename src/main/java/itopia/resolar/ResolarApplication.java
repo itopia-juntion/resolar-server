9d29e930212678bc5df05c1ebc0246a81f2226bf
klaus9267 <klaus9267@gmail.com>
2025-08-23 20:20:24 +0900

    initˆ

---
+++ src/main/java/itopia/resolar/ResolarApplication.java
@@ -0,0 +1,13 @@
+package itopia.resolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itopia/resolar/ResolarApplicationTests.java
@@ -0,0 +1,13 @@
+package itopia.resolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
